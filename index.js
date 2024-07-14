3dc20f5b46fd33395bab1e16e9d8d12661eae457
Arjun Lama <arjunlama4268@gmail.com>
2024-07-14 21:30:47 +0545

    fix cors

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
+require("./config/database");
+require("dotenv").config();
+const cors = require("cors");
+const PORT = process.env.PORT;
+const { handleServererro, resourceNotfound } = require("./middleware/error");
+const router = require("./routes/auth");
+const express = require("express");
+const app = express();
 
-require("./config/database")
-require('dotenv').config()
-const PORT=process.env.PORT
-const { handleServererro, resourceNotfound } = require("./middleware/error")
-const router=require("./routes/auth")
-const express=require("express")
-const app=express()
-app.get("/",(req,res)=>{
-res.send("Your Server is alive")
-})
-app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("Your Server is alive");
+});
+app.use(express.json());
+app.use(cors());
+app.use("/api/", router);
 
-app.use("/api/",router)
-
-
-
-
-
-app.use(resourceNotfound)
-app.use(handleServererro)
-app.listen(PORT,()=>{
-console.log(`The School server is Listening to Port ${PORT}`)
-})
+app.use(resourceNotfound);
+app.use(handleServererro);
+app.listen(PORT, () => {
+  console.log(`The School server is Listening to Port ${PORT}`);
+});
