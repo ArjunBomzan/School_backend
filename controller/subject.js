b8c5e5dd0309c359b43e339a33fcc1a813a9d703
Arjun Lama <arjunlama4268@gmail.com>
2024-09-19 11:46:20 +0545

    fix

--- controller/subject.js
+++ controller/subject.js
@@ -1,4 +1,5 @@
 const Subject = require("../Schema/Subject");
+const mongoose = require("mongoose");
 async function createSubject(req, res, next) {
   try {
     data = await Subject(req.body).save();
@@ -9,9 +10,36 @@ async function createSubject(req, res, next) {
 }
 async function getSubjectBySemester(req, res, next) {
   const semesterId = req.params.id;
-  try {
-    data = await Subject.find({ semesterId: semesterId });
 
+  try {
+    data = await Subject.aggregate([
+      {
+        $match: { semesterId: new mongoose.Types.ObjectId(semesterId) },
+      },
+      {
+        $lookup: {
+          from: "chapters",
+          localField: "_id",
+          foreignField: "subjectId",
+          as: "chapters",
+        },
+      },
+      {
+        $addFields: {
+          chapterCount: { $size: "$chapters" },
+        },
+      },
+      {
+        $project: {
+          name: 1,
+          description: 1,
+          code: 1,
+          semesterId: 1,
+          chapterCount: 1,
+        },
+      },
+    ]);
+    console.log(data);
     res.status(200).send(data);
   } catch (err) {
     next(err);
